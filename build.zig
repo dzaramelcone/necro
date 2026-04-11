145a59fa5d80a2d23e9c9d4feae9fe245edeb4c5
Dzara Melcone <dzaramelcone@gmail.com>
2026-04-11 12:57:42 -0400

    build.zig

--- build.zig
+++ build.zig
@@ -36,13 +36,15 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
-            .strip = true,
+            .strip = optimize != .Debug,
         }),
     });
     const options = b.addOptions();
     options.addOption(bool, "metrics", metrics);
     pyext.root_module.addOptions("build_options", options);
-    pyext.want_lto = true;
+    pyext.root_module.addImport("necro", pyext.root_module);
+    const os = target.result.os.tag;
+    pyext.lto = if (os == .macos) null else .full;
     pyext.link_gc_sections = true;
     linkPython(pyext.root_module);
     b.installArtifact(pyext);
